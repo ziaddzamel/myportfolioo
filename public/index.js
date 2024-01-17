c5ace25ff7ee63322bee674722c54ba488d58c7a
ziaddzamel <113547181+ziaddzamel@users.noreply.github.com>
2024-01-17 03:55:58 +0200

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import demo from "./demo.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import chakra from "./tech/chakra.png";
+import next from "./tech/nextjs.svg";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import Gpt3 from "./mywork/Gpt3.png";
+import Todo from "./mywork/Todo.png";
+import snapgram from "./mywork/SnapGram.png";
+import bookmark from "./mywork/BookMark.png";
+import Travel from "./mywork/Travel.png";
+export {
+  Todo,
+  bookmark,
+  Gpt3,
+  Travel,
+  snapgram,
+  chakra,
+  logo,
+  next,
+  demo,
+  menu,
+  close,
+  css,
+  figma,
+  reactjs,
+  git,
+  html,
+  javascript,
+  tailwind,
+  typescript,
+  threejs,
+};
